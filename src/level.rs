2d6c25fd26968cf9f4dcb0de62484390ec01e542
Ambareesh Shyam Sundar <asundar44@gatech.edu>
2024-07-21 15:52:46 -0700

    add physics and jumping

--- src/level.rs
+++ src/level.rs
@@ -1,5 +1,7 @@
+use std::time::Duration;
 use bevy::prelude::*;
-use bevy_ecs_ldtk::{LdtkPlugin, LdtkSpriteSheetBundle, LdtkEntity, LdtkWorldBundle, app::LdtkEntityAppExt, LevelSelection};
+use bevy_rapier2d::prelude::*;
+use bevy_ecs_ldtk::prelude::*;
 
 pub struct LevelManagementPlugin;
 
@@ -8,9 +10,69 @@ impl Plugin for LevelManagementPlugin {
             app.add_plugins(LdtkPlugin)
                 .insert_resource(LevelSelection::index(0))
                 .register_ldtk_entity::<PlayerBundle>("Player")
-                .add_systems(Startup, spawn_level);
+                .register_ldtk_int_cell::<TerrainBundle>(1)
+                .add_systems(Startup, spawn_level)
+                .add_systems(Update, add_collider)
+                .add_systems(Update, update_player_grounded)
+                .add_systems(Update, move_player);
     }
 }
+
+fn add_collider(mut commands: Commands, query: Query<Entity, Added<PlayerMarker>>) {
+    if let Ok(entity) = query.get_single() {
+        println!("found player entity");
+        commands.entity(entity).with_children(|parent| {
+            parent.spawn((
+                Collider::round_cuboid(4., 2., 2.),
+                Sensor,
+                ActiveEvents::COLLISION_EVENTS,
+                TransformBundle::from_transform(Transform::from_xyz(0., -4.2, 0.)),
+                PlayerJumpColliderMarker,
+            ));
+        });
+    }
+}
+
+fn update_player_grounded(
+    mut collision_events: EventReader<CollisionEvent>,
+    query_player_jump_collider: Query<Entity, With<PlayerJumpColliderMarker>>,
+    mut query_player: Query<&mut PlayerStatus, With<PlayerMarker>>,
+    rapier_context: Res<RapierContext>,
+) {
+    if let Ok(player_jump_controller_entity) = query_player_jump_collider.get_single() {
+        if let Ok(mut player_status) = query_player.get_single_mut() {
+            // let grounded = false;
+            for collision_event in collision_events.read() {
+                match collision_event {
+                    CollisionEvent::Started(entity_1, entity_2, _) => {
+                        if *entity_1 == player_jump_controller_entity || *entity_2 == player_jump_controller_entity {
+                            player_status.grounded = true;
+                            println!("grounded");
+                            break;
+                        }
+                    }
+                    CollisionEvent::Stopped(entity_1, entity_2, _) => {
+                        if (*entity_1 == player_jump_controller_entity || *entity_2 == player_jump_controller_entity) && rapier_context.intersection_pairs_with(player_jump_controller_entity).peekable().peek() == None {
+                            player_status.grounded = false;
+                            println!("ungrounded");
+                        } else {
+                            println!("collision stopped but not ungrounded");
+                            println!("{:?}", rapier_context.intersection_pairs_with(player_jump_controller_entity).peekable().peek().unwrap());
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+// fn update_player_grounded(mut query_player: Query<&mut PlayerStatus, With<PlayerMarker>>, query_player_jump_collider: Query<Entity, With<PlayerJumpColliderMarker>>, rapier_context: Res<RapierContext>,) {
+//     if let Ok(player_jump_collider_entity) = query_player_jump_collider.get_single() {
+//         if let Ok(mut player_status) = query_player.get_single_mut() {
+//             player_status.grounded = rapier_context.intersection_pairs_with(player_jump_collider_entity).peekable().peek() != None;
+//         }
+//     }
+// }
 
 fn spawn_level(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(LdtkWorldBundle {
@@ -19,8 +81,104 @@ fn spawn_level(mut commands: Commands, asset_server: Res<AssetServer>) {
     });
 }
 
-#[derive(Default, Bundle, LdtkEntity)]
+fn move_player(
+    mut query_player: Query<(Entity, &mut Velocity, &Transform, &mut PlayerStatus), With<PlayerMarker>>,
+    keys: Res<ButtonInput<KeyCode>>,
+    rapier_context: Res<RapierContext>,
+    time: Res<Time>,
+) {
+    if let Ok((player_entity, mut player_velocity, transform, mut player_status)) = query_player.get_single_mut() {
+        if !player_status.jump_cooldown.finished() {
+            player_status.jump_cooldown.tick(time.delta());
+        }
+        // player_velocity.linvel = Vec2::ZERO;
+        if keys.pressed(KeyCode::ArrowRight) {
+            player_velocity.linvel += 35. * Vec2::X;
+        }
+        if keys.pressed(KeyCode::ArrowLeft) {
+            player_velocity.linvel -= 35. * Vec2::X;
+        }
+        if keys.pressed(KeyCode::ArrowUp) && player_status.grounded {
+            // ugly but i wrote it like this so i can print debug messages
+            if player_status.jump_cooldown.finished() {
+                player_velocity.linvel += 125. * Vec2::Y;
+                player_status.grounded = false;
+                player_status.jump_cooldown.reset();
+            }
+        }
+
+        player_velocity.linvel.x /= 1.6;
+    }
+}
+
+#[derive(Component)]
+struct PlayerJumpColliderMarker;
+
+#[derive(Default, Component)]
+struct PlayerMarker;
+
+#[derive(Component)]
+struct PlayerStatus {
+    grounded: bool,
+    jump_cooldown: Timer,
+    air_jumps: usize,
+    max_air_jumps: usize,
+}
+
+#[derive(Bundle, LdtkEntity)]
 struct PlayerBundle {
     #[sprite_sheet_bundle]
     sprite_sheet_bundle: LdtkSpriteSheetBundle,
+    player_marker: PlayerMarker,
+    player_status: PlayerStatus,
+    rigid_body: RigidBody,
+    collider: Collider,
+    velocity: Velocity,
+    friction: Friction,
+    locked_axes: LockedAxes,
+}
+
+impl Default for PlayerBundle {
+    fn default() -> Self {
+        let mut jump_cooldown_timer = Timer::new(Duration::from_millis(300), TimerMode::Once);
+        jump_cooldown_timer.tick(Duration::from_millis(300));
+        Self {
+            sprite_sheet_bundle: LdtkSpriteSheetBundle::default(),
+            player_marker: PlayerMarker,
+            player_status: PlayerStatus {
+                grounded: true,
+                jump_cooldown: jump_cooldown_timer,
+                air_jumps: 1,
+                max_air_jumps: 1,
+            },
+            rigid_body: RigidBody::Dynamic,
+            collider: Collider::round_cuboid(6., 6., 2.),
+            velocity: Velocity::default(),
+            friction: Friction {
+                coefficient: 0.,
+                combine_rule: CoefficientCombineRule::Min,
+            },
+            locked_axes: LockedAxes::ROTATION_LOCKED,
+        }
+    }
+}
+
+#[derive(Default, Component)]
+struct TerrainMarker;
+
+#[derive(Bundle, LdtkIntCell)]
+struct TerrainBundle {
+    terrain_marker: TerrainMarker,
+    rigid_body: RigidBody,
+    collider: Collider,
+}
+
+impl Default for TerrainBundle {
+    fn default() -> Self {
+        Self {
+            terrain_marker: TerrainMarker,
+            rigid_body: RigidBody::Fixed,
+            collider: Collider::round_cuboid(7., 7., 1.),
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 mod camera;
 mod level;
 
-use bevy::prelude::*;
+use bevy::{ecs::schedule::{LogLevel, ScheduleBuildSettings}, prelude::*};
 use bevy_rapier2d::prelude::*;
 use camera::CameraManagementPlugin;
 use level::LevelManagementPlugin;
 
 fn main() {
     App::new()
+        // Enable ambiguity warnings for the Update schedule
+        .edit_schedule(Startup, |schedule| {
+            schedule.set_build_settings(ScheduleBuildSettings {
+                ambiguity_detection: LogLevel::Warn,
+                ..default()
+            });
+        })
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
-        .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.))
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(24.))
+        .add_plugins(RapierDebugRenderPlugin::default())
         .add_plugins((
             CameraManagementPlugin,
             LevelManagementPlugin,
